a9943135c1ea68e3eb920a8e7d361d927766fd5c
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-05-03 21:58:19 +0900

    今日の一問削除

--- src/config/links.tsx
+++ src/config/links.tsx
@@ -5,5 +5,4 @@ import { FiTag } from 'react-icons/fi';
 export const links: LinkType[] = [
   { name: 'tags', href: '/tags', icon: <FiTag /> },
   { name: 'about', href: '/about', icon: <AiOutlineInfoCircle /> },
-  { name: '今日の一問', href: '/tags/今日の一問', icon: <AiOutlineInfoCircle /> },
 ];
